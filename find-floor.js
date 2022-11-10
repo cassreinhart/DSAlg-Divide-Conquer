bc4cbeaf7bcde09d84d457390cb20afb2dc86db5
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-11-10 11:47:35 -0600

    updated solutions

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,17 @@
-function findFloor() {
-  
+function findFloor(array, x) {
+    let left = 0;
+    let right = array.length - 1;
+
+    while (right >= left) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === x) return arr[mid];
+        else if (arr[mid] > x) {
+            right = mid - 1;
+        } else if (arr[mid] < x) {
+            left = mid + 1;
+        } else if (arr[left] > x) return -1;
+    } return mid;
 }
 
 module.exports = findFloor
--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,19 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, num) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((right + left) / 2);
+
+        if (arr[mid] === num) return mid;
+        else if (arr[right] > num) {
+            left = mid + 1;
+        } else if (arr[mid] > num) {
+            right = mid - 1;
+        } else if (arr[mid] < num) {
+            left = mid + 1;
+        } 
+    } return - 1;
 }
 
 module.exports = findRotatedIndex
