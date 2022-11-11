afbf1586016bc3cbbee43fb6af7722bc73945ea5
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-11-10 22:43:49 -0600

    Initial Solutions

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,25 @@
-function countZeroes() {
-  
+function countZeroes(array) {
+  let rightIdx = array.length - 1;
+  let leftIdx = 0;
+
+  if (array[rightIdx] === 0 && array[leftIdx] === 0) return array.length;
+  else if (array[rightIdx] === 1) return 0;
+
+  while (leftIdx <= rightIdx) {
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    
+    if (array[middleIdx] === 1) {
+        leftIdx = middleIdx + 1;
+        //check left until 0
+    } else if (array[middleIdx]  === 0 && array[middleIdx - 1] === 1) {
+        return array.length - middleIdx;
+        // check right until 1
+    } else {
+        rightIdx = middleIdx - 1;
+    }
+  } 
 }
 
 module.exports = countZeroes
+L .      R
+[1,0,0,0,0]
--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,18 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+
+        if (arr[mid] < arr[mid - 1]) return mid;
+        else if (arr[left] > arr[left + 1]) return arr[left + 1];
+        else if (arr[right] < arr[right - 1]) return arr[right - 1];
+        else {
+            left ++;
+            right--;
+        }
+    }
 }
 
 module.exports = findRotationCount
